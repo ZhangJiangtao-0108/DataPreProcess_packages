a3d5115e431238815f6a791eaf84ef2a880d1905
ZhangJiangtao-0108 <775065019@qq.com>
2021-11-23 22:40:02 +0800

    The installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name="DataPreProce",
+    version="1.0",
+    description="The data collected by MYO bracelet is preprocessed, including data cutting, stretching, filling and feature extraction",
+    author="ZhangJiangtao",
+    py_modules=["packages.DataPre",\
+                "packages.makedatasets",\
+                "packages.algorithm.Attitude_Angle_solution",\
+                "packages.algorithm.cutting_algorithm",\
+                "packages.algorithm.emg_correct"]
+)
