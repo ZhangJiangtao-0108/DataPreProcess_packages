159f589657218c6bc607c8f8f4b4e6f1cb059580
ZhangJiangtao-0108 <775065019@qq.com>
2021-11-23 22:41:47 +0800

    __init__

---
+++ packages/__init__.py
@@ -0,0 +1,14 @@
+## 数据预处理的包
+# __init.py
+'''
+    可以在这个文件中批量导入所需要的模块
+'''
+import numpy as np
+import os
+import DataPre
+
+__all__ = ["DataPre",\
+            "makedatasets",\
+            "algorithm.Attitude_Angle_solution",\
+            "algorithm.cutting_algorithm",\
+            "algorithm.emg_correct"]
