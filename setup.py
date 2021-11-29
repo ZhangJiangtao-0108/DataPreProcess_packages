ac42a03bef0fe9ca7d0abe49cca1e58772c842ef
ZhangJiangtao-0108 <775065019@qq.com>
2021-11-29 21:32:12 +0800

    Add some information

--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@ setup(
     name="DataPreProce",
     version="1.0",
     description="The data collected by MYO bracelet is preprocessed, including data cutting, stretching, filling and feature extraction",
+    url="https://github.com/ZhangJiangtao-0108/DataPreProcess_packages.githttps://github.com/TangSir61/UIMonkey2021.git",
     author="ZhangJiangtao",
-    py_modules=["packages.DataPre",\
-                "packages.makedatasets",\
-                "packages.algorithm.Attitude_Angle_solution",\
-                "packages.algorithm.cutting_algorithm",\
-                "packages.algorithm.emg_correct",\
-                "oackages.algorithm.emg_feature"]
+    author_email="zjt0108@foxmail.com",
+    py_modules=["DataPreProce.DataPre",\
+                "DataPreProce.makedatasets",\
+                "DataPreProce.algorithm.Attitude_Angle_solution",\
+                "DataPreProce.algorithm.cutting_algorithm",\
+                "DataPreProce.algorithm.emg_correct",\
+                "DataPreProce.algorithm.emg_feature",\
+                "DataPreProce.test"],
+    packages = ['DataPreProce'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'DataPreProce = DataPreProce.__main__:main'
+        ]
+    }
 )
