7883db78ec2578cae96661f09126164e48556be1
ZhangJiangtao-0108 <775065019@qq.com>
2021-12-20 22:37:20 +0800

    add wave Filter

--- DataPreProce/algorithm/Butter_filter.py
+++ DataPreProce/algorithm/Butter_filter.py
@@ -2,6 +2,8 @@
 定义滤波器
 '''
 from scipy import signal
+import pywt
+
 def butter_filter(data, category = 'lowpass', order = 8, Wn = 0.5, axis = 0):
     '''巴特沃斯低通滤波器
 
@@ -13,3 +15,20 @@ def butter_filter(data, category = 'lowpass', order = 8, Wn = 0.5, axis = 0):
     b, a = signal.butter(order, Wn, category)   #配置滤波器 8 表示滤波器的阶数
     filtedData = signal.filtfilt(b, a, data, axis=axis)  #data为要过滤的信号
     return filtedData
+
+def Wave_filter(data, w):
+    '''利用小波变换将数据进行重构，达到滤波效果
+
+        args：
+            data：需要滤波的数据
+            w：选取的小波函数
+    '''
+    mode = pywt.Modes.smooth
+    W = pywt.Wavelet(w)
+    a = data
+    (a, d) = pywt.dwt(a, W, mode)
+    coeff_list = [a, None]
+    rec = pywt.waverec(coeff_list, w) 
+    return rec
+    
+    
