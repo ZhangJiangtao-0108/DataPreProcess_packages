55d48e3e0411c4defbe93b54f9f69af91362e12a
ZhangJiangtao-0108 <775065019@qq.com>
2023-06-05 21:31:13 +0800

    Sentence Compare

---
+++ DataStatistics/utils/SentenceCompare.py
@@ -0,0 +1,24 @@
+
+def SentenceCompare(Template_Sentences_list, Compare_Sentences_list, neglect:list):
+    if len(Template_Sentences_list) != len(Compare_Sentences_list):
+        raise IndexError("句子条数不匹配")
+    
+    Template_Gesture_Sta = {}
+    Correct_Gesture_sta = {}
+    Template_Gestures = []
+    Correct_Gestures = []
+    for i in range(len(Compare_Sentences_list)):
+        Template_Gestures += Template_Sentences_list[i]
+        for gesture in Compare_Sentences_list[i]:
+            if (gesture not in neglect) and (gesture in Template_Sentences_list[i]):
+                Correct_Gestures.append(gesture)
+        
+    Template_Gestures_set = set(Template_Gestures)
+    for gesture in Template_Gestures_set:
+        Template_Gesture_Sta[gesture] = Template_Gestures.count()
+
+    Correct_Gestures_set = set(Correct_Gestures)
+    for gesture in Correct_Gestures_set:
+        Correct_Gesture_sta[gesture] = Correct_Gestures.count()
+
+    return Template_Gesture_Sta, Correct_Gesture_sta
