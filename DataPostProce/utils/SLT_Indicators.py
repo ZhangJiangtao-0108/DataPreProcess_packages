9a6a8c90219fcff998a97c73d54f1b0e11e03ce5
ZhangJiangtao-0108 <775065019@qq.com>
2023-10-17 16:28:12 +0800

    Modify

--- DataPostProce/utils/SLT_Indicators.py
+++ DataPostProce/utils/SLT_Indicators.py
@@ -3,13 +3,13 @@ from utils.Labels2Text import geatures2text
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 
 
-def compute_SLT_Indicators(sentence_candidate_list, Sentence, pre_label):
+def compute_SLT_Indicators(Sentence, pre_label, sentence_candidate_list=None):
     '''
         sentence_candidate_list:Spoken句子列表集合[batch_size, candidate_num, sentence_len]
         sentence:句子列表[batch_size, sentence_len]
         pre_label:预测句子列表[batch_size, sentence_len]
     '''
-    corpus_bleu_score = corpus_bleu(sentence_candidate_list, pre_label)
+   
     sentence_bleu_score = sentence_bleu(Sentence, pre_label)
     sentence_bleu1_score = sentence_bleu(Sentence, pre_label, weights=(1, 0, 0, 0))
     sentence_bleu2_score = sentence_bleu(Sentence, pre_label, weights=(0, 1, 0, 0))
@@ -20,4 +20,8 @@ def compute_SLT_Indicators(sentence_candidate_list, Sentence, pre_label):
         _,_,_, WER = computeWer(geatures2text(Sentence[b]), geatures2text(pre_label[b]))
         wer += WER
     wer = wer/len(Sentence)
-    return corpus_bleu_score, sentence_bleu_score, sentence_bleu1_score, sentence_bleu2_score, sentence_bleu3_score, sentence_bleu4_score, wer
+    if sentence_candidate_list != None:
+        corpus_bleu_score = corpus_bleu(sentence_candidate_list, pre_label)
+        return corpus_bleu_score, sentence_bleu_score, sentence_bleu1_score, sentence_bleu2_score, sentence_bleu3_score, sentence_bleu4_score, wer
+    else:
+        return sentence_bleu_score, sentence_bleu1_score, sentence_bleu2_score, sentence_bleu3_score, sentence_bleu4_score, wer
