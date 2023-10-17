d55338e3f491af6144824d96f68ec9e86f4aa6da
ZhangJiangtao-0108 <775065019@qq.com>
2023-10-17 22:24:58 +0800

    进行格式纠正

--- DataPostProce/utils/SLT_Indicators.py
+++ DataPostProce/utils/SLT_Indicators.py
@@ -6,17 +6,25 @@ from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 def compute_SLT_Indicators(Sentence, pre_label, sentence_candidate_list=None):
     '''
         sentence_candidate_list:Spoken句子列表集合[batch_size, candidate_num, sentence_len]
-        sentence:句子列表[batch_size, sentence_len]
+        sentence:句子列表[batch_size, 1, sentence_len]
         pre_label:预测句子列表[batch_size, sentence_len]
     '''
-   
-    sentence_bleu_score = sentence_bleu(Sentence, pre_label)
-    sentence_bleu1_score = sentence_bleu(Sentence, pre_label, weights=(1, 0, 0, 0))
-    sentence_bleu2_score = sentence_bleu(Sentence, pre_label, weights=(0, 1, 0, 0))
-    sentence_bleu3_score = sentence_bleu(Sentence, pre_label, weights=(0, 0, 1, 0))
-    sentence_bleu4_score = sentence_bleu(Sentence, pre_label, weights=(0, 0, 0, 1))
-    wer = 0
+    
+    # print(Sentence)
+    # print(pre_label)
+    sentence_bleu_score = 0.0
+    sentence_bleu1_score = 0.0
+    sentence_bleu2_score = 0.0
+    sentence_bleu3_score = 0.0
+    sentence_bleu4_score = 0.0
+    wer = 0.0
     for b in range(len(Sentence)):
+        sentence_bleu_score += sentence_bleu([Sentence[b]], pre_label[b])
+        sentence_bleu1_score += sentence_bleu([Sentence[b]], pre_label[b], weights=(1, 0, 0, 0))
+        sentence_bleu2_score += sentence_bleu([Sentence[b]], pre_label[b], weights=(0, 1, 0, 0))
+        sentence_bleu3_score += sentence_bleu([Sentence[b]], pre_label[b], weights=(0, 0, 1, 0))
+        sentence_bleu4_score += sentence_bleu([Sentence[b]], pre_label[b], weights=(0, 0, 0, 1))
+    
         _,_,_, WER = computeWer(geatures2text(Sentence[b]), geatures2text(pre_label[b]))
         wer += WER
     wer = wer/len(Sentence)
